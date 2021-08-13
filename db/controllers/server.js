b65ddabe2dfe27a324931001e679cab713468bb9
gamerfriend <aesemichev@edu.hse.ru>
2021-08-13 09:56:17 +0300

    add new working files

--- db/controllers/server.js
+++ db/controllers/server.js
@@ -1,28 +1,24 @@
-let dbs = [
+// export  const getAll = (req,res) => {
+//     const dbs = await user.findMany({
+//         select:{
+//             id: true,
+//             username:true,
+//             posts:true
+//         }
+//     });
+//     res.status(200).json(dbs)
+// }
+//
+// export const create = (req,res) => {
+//     const newServer = {
+//         id: Date.now().toString(),
+//         ...req.body
+//     }
+//     dbs.push(newServer)
+//     res.status(201).json(newServer)
+// }
 
-]
-
-let lists = [
-
-]
-let objects = [
-
-]
-
-export  const getAll = (req,res) => {
-    res.status(200).json(servers)
-}
-
-export const create = (req,res) => {
-    const newServer = {
-        id: Date.now().toString(),
-        ...req.body
-    }
-    servers.push(newServer)
-    res.status(201).json(newServer)
-}
-
-export const remove =(req,res) =>{
-    servers = servers.filter(s => s.id !== req.params.id)
-    res.json({messege : 'server ${req.params.id} has benn removed '})
-}
+// export const remove =(req,res) =>{
+//     servers = servers.filter(s => s.id !== req.params.id)
+//     res.json({messege : 'server ${req.params.id} has benn removed '})
+// }
